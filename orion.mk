3a97b0a88f05a4dc2561e535e24ba60cb7ff89bb
TJSteveMX <ec.lento@gmail.com>
2016-07-05 08:00:07 -0700

    MTC19V

--- orion.mk
+++ orion.mk
@@ -18,7 +18,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MMB29Q/2480792:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MMB29Q 2480792 release-keys" \
-    BUILD_ID=MMB29Q
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC19V/2862947:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC19V 2862947 release-keys" \
+    BUILD_ID=MTC19V
 
